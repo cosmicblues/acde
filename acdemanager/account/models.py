3a656accedee28c0ea534e08cc9f772542ff4c1a
cosmicblues <theo.benmoussa@epitech.eu>
2021-04-26 19:05:14 +0200

    création models bdd

--- acdemanager/account/models.py
+++ acdemanager/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(max_length=200, null=True)
+    lastName = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
