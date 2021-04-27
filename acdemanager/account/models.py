83ee1a562b1e7798288e3cceb1f80e7b9419b638
cosmicblues <theo.benmoussa@epitech.eu>
2021-04-27 16:12:32 +0200

    générateur de données en cours + model table user

--- acdemanager/account/models.py
+++ acdemanager/account/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    firstName = models.CharField(max_length=200, null=True)
-    lastName = models.CharField(max_length=200, null=True)
+    Name = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
--- data_generator.py
+++ data_generator.py
@@ -1,11 +1,33 @@
 import names
 
-first_name_list = []
-last_name_list = []
+full_name_list = []
 
 for a in range(0, 1001):
-    first_name_list.append(names.get_first_name())
-    last_name_list.append(names.get_last_name())
+    full_name_list.append(names.get_full_name())
 
-print(first_name_list)
-print(last_name_list)
+print(full_name_list)
+
+#import random
+#domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
+#letters = string.ascii_lowercase[:12]
+
+#def get_random_domain(domains):
+    #return random.choice(domains)
+
+#def get_random_name(letters, length):
+    #return ''.join(random.choice(letters) for i in range(length))
+
+#def generate_random_emails(nb, length):
+    #return [get_random_name(letters, length) + '@' + get_random_domain(domains) for i in range(nb)]
+
+#def main():
+    #print(generate_random_emails(10, 7))
+
+#if __name__ == "__main__":
+    #main()
+ 
+#import string
+
+#for i in range(3):
+    #result_str = ''.join(random.sample(string.ascii_lowercase, 8))
+    #print(result_str)
